ae5f34f9eee2f600198f22febd6beeea95f42f8c
Bethel23 <sorochivic23@gmail.com>
2023-02-23 05:53:16 -0800

    2 multiplier

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - Muitiplies two integers and return their product
+ *
+ * @a: integer to be added
+ *
+ * @b: integer to be added
+ *
+ * Return: result
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return result;
+}
