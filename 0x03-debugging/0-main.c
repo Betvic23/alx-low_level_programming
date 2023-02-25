c3740d4f6eef5af5a7bd320af6d939d4b4c14bfb
Bethel23 <sorochivic23@gmail.com>
2023-02-25 06:39:38 -0800

    debugging one

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - shows numbers that are negative or positive
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
