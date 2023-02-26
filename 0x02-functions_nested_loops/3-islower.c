b688b7e04e5b47778525fd9d01fd55c55cf87fdb
Bethel23 <sorochivic23@gmail.com>
2023-02-26 14:45:00 -0800

    new is lower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- * _islower - checks for lowercase character
- * Return: 1 if letter is lowercase, else return 0
- * 'C' - character
+ * _islower - checks if a character is lower
+ * @c: character
+ * Return: 1 if lower else 0
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
