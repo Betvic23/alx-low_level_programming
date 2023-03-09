7e26c6a1321dc3208c453d3fccb1fc3e4c82f070
Bethel23 <sorochivic23@gmail.com>
2023-03-09 09:36:09 -0800

    print rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: character
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		return;
+	}
+	_putchar(*s);
+	_print_rev_recursion(s + 1);
+}
