ed7bfcf1492a80cfe0c3200d3fb0975c0e82826f
Bethel23 <sorochivic23@gmail.com>
2023-03-13 14:32:46 -0700

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - entry
+ * @argc: argument
+ * @argv: argument counter
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s", argv[0]);
+	return (0);
+}
