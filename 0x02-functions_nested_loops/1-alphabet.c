a3502a6f0a9e159a525f62f7b66634af7ba266a5
Bethel23 <sorochivic23@gmail.com>
2023-06-21 04:26:53 -0700

    alphabets in small

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * print_alphabet - prints alphabets in small letters
  */
 void print_alphabet(void)
 {
 	int i;
 
-	for (i = 'a'; i < 'z'; i++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 	}
