4ee00a824f69e285870a9e9b25fd45573144a6a2
Bethel23 <sorochivic23@gmail.com>
2023-06-21 15:34:50 -0700

    putchar and _putchar differs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_sign - prints the sign of a number
+ * @n: int value
+ * Return: 1 or 0, else -1
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	return (n);
+}
