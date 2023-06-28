52b522220aa5aef30508548f3920ebf69a03412a
Bethel23 <sorochivic23@gmail.com>
2023-06-28 16:10:53 -0700

    puts 2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * puts2 - prints half a number provided
+ * @str: char pointer
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] % 2 == 0)
+		{
+			_putchar(str[i]);
+			str++;
+		}
+	}
+	_putchar('\n');
+}
