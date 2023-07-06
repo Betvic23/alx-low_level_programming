88a56ec0af858629682d4b47485ed1e472c6a45d
Bethel23 <sorochivic23@gmail.com>
2023-07-06 15:58:48 -0700

    palindrome

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+int is_pal_helper(char *s, int a, int b);
+/**
+ * is_palindrome - checks for a palindrome
+ * @s: char pointer
+ * Return: is_pal_helper
+ */
+int is_palindrome(char *s)
+{
+	int len = strlen(s);
+	return is_pal_helper(s, 0, len-1);
+}
+
+/**
+ * is_pal_helper - helps checks for the palindrome
+ * @a: int value
+ * @b: int value
+ * Return: 0
+ */
+int is_pal_helper(char *s, int a, int b)
+{
+	if (a >= b)
+	{
+		return (1);
+	}
+
+	if (s[a] == s[b])
+	{
+		return is_pal_helper(s, a+1, b-1);
+	}
+	return (0);
+}
