c74e6cd5435570796533f65112dc60597ee6325b
Bethel23 <sorochivic23@gmail.com>
2023-07-12 14:39:33 -0700

    what is myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - entry
+ * @argc: arguments
+ * @argv: arg array
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
