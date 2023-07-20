c01508c01a2bb365b47636675d25b18cac8ce24d
Bethel23 <sorochivic23@gmail.com>
2023-07-19 17:26:58 -0700

    int index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+/**
+ * int_index - searches for an int
+ * @array: array
+ * @cmp: pointer to function
+ * @size: number of array element
+ * Return: -1
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int a;
+
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (a = 0; a < size; a++)
+		if (cmp(array[a]))
+		{
+			return (a);
+		}
+	return (-1);
+}
